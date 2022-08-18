26cfea69eb0988eeee144a0ee742d5f7e6c2369b
Thomas Mitchelmore <tommitchelmore@outlook.com>
2022-08-18 12:10:05 +0100

    feat: update code to produce n outputs

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,10 @@ import {TInput, InputType} from "./InputType";
 import { createSpinner } from "nanospinner";
 import chalk from "chalk";
 
-async function handleCreate({i,o}: yargs.ArgumentsCamelCase) {
+async function handleCreate({i,d,p}: yargs.ArgumentsCamelCase) {
     const input = String(i)
-    const output = String(o)
+    const output = String(d)
+    const prefix = String(p)
 
 
     const fileSpinner = createSpinner("Attempting to read input file")
@@ -27,29 +28,43 @@ async function handleCreate({i,o}: yargs.ArgumentsCamelCase) {
         }
         fileSpinner.success({ text: "Successfully loaded JSON data" })
         
-        const { variables, elements } = JSON.parse(file) as TInput;
-    
-        let loadedSvgs = Promise.all(elements.map(async el => {
-            const spinner = createSpinner(`Loading ${chalk.yellow(el + ".svg")}`)
-            try {
-                const svg = await loadSvg(path.join(__dirname, "..", "elements", el + ".svg"))
-                spinner.success({ text: `Loaded ${chalk.yellow(el + ".svg")}` })
-                return svg
-            } catch (e) {
-                spinner.error({ text: `Could not load ${chalk.redBright(el + ".svg")}` })
+        const arr = JSON.parse(file) as TInput;
+
+        const dirSpinner = createSpinner("Attempting to create output directory")
+        try {
+            fs.mkdirSync(output)
+        } catch (e) {
+            if (e.code !== "EEXIST") {
+                dirSpinner.error({ text: `Could not create output directory ${chalk.redBright(output)} - printing additional info below:` })
                 console.log(e)
                 process.exit(1)
             }
+        }
+        dirSpinner.success({ text: "Successfully created output directory" })
 
+        Promise.all(arr.map(async ({name, variables, elements}, idx) => {
+            const spinner = createSpinner(`Building ${name || idx}`)
+            let loadedSvgs = await Promise.all(elements.map(async el => {
+                spinner.update({text: `Building ${name || idx}: loading ${chalk.yellow(el)}`})
+                try {
+                    const svg = await loadSvg(path.join(__dirname, "..", "elements", el + ".svg"))
+                    return svg
+                } catch(e) {
+                    spinner.error({text: `Could not load ${chalk.redBright(el)}.svg`})
+                    process.exit(1)
+                }
+            }))
+
+            spinner.update({text: `Building ${name || idx}: Creating output file`})
+            await createNewSvg(path.join(output, prefix ? prefix + "-" + (name || idx) + ".svg" : (name || idx) + ".svg"), variables, ...loadedSvgs)
+            spinner.success({text: `Building ${name || idx}: Successfully created output file ${chalk.yellow(path.join(output, prefix ? prefix + "-" + (name || idx) + ".svg" : (name || idx) + ".svg"))}`})
         }))
-    
-        const svgSpinner = createSpinner(`Creating new SVG`)
-        createNewSvg(output, variables, ...(await loadedSvgs))
-        svgSpinner.success({ text: `Created new SVG: ${chalk.green(output)}` })
     } catch (e) {
         fileSpinner.error({text: `Could not read input file ${chalk.redBright(input)}`})
         process.exit(1)
     }
+
+
 }
 
 async function run() {
@@ -60,11 +75,17 @@ async function run() {
             describe: "Create a new svg file",
             aliases: ["c"],
             builder: {
-                "Output File": {
-                    describe: "The name of the file to create",
+                "Output Directory": {
+                    describe: "The name of the folder to create",
                     demandOption: true,
                     type: "string",
-                    alias: "o",
+                    alias: "d",
+                },
+                "File Prefix": {
+                    describe: "The prefix for each file",
+                    demandOption: false,
+                    type: "string",
+                    alias: "p"
                 },
                 "Input File": {
                     describe: "Path to the input file",
