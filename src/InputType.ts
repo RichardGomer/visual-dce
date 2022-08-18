dc0eb37be0a68d6a0fddd846213bf32a661793b6
Thomas Mitchelmore <tommitchelmore@outlook.com>
2022-08-18 11:19:52 +0100

    feat: update input type to be an array

--- src/InputType.ts
+++ src/InputType.ts
@@ -1,9 +1,10 @@
 import { z } from "zod"
 
-const InputType = z.object({
+const InputType = z.array(z.object({
     variables: z.record(z.string(), z.string()),
-    elements: z.array(z.string())
-})
+    elements: z.array(z.string()),
+    name: z.string().optional(),
+}))
 
 type TInput = z.infer<typeof InputType>
 
