e0102b9353accb75dc7ae0f0a64e76521275bb04
Thomas Mitchelmore <tommitchelmore@outlook.com>
2022-08-18 10:15:27 +0100

    feat: add new elements

--- src/createNewSvg.ts
+++ src/createNewSvg.ts
@@ -2,7 +2,7 @@ import { JSDOM } from 'jsdom';
 import fs from 'fs';
 
 async function createNewSvg(filename: string, variables: Record<string, string>, ...parts: HTMLCollection[]): Promise<void> {
-    const doc = new JSDOM(`<svg xmlns="http://www.w3.org/2000/svg" width="800" height="600" viewBox="0 0 800 600" fill="none" xmlns="http://www.w3.org/2000/svg"></svg>`);
+    const doc = new JSDOM(`<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="800" height="600" viewBox="0 0 800 600" fill="none"></svg>`);
     const svg = doc.window.document.querySelector("svg") as SVGSVGElement;
     if (!svg) throw new Error("Could not find svg");
 
